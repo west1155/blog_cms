7cf05f3042a92f21fed9979bfc9cc343a18f933a
west1155 <westsupport@gmail.com>
2025-03-07 06:16:35 +0000

    latest

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,20 +2,38 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import {
-    PostDetail,
+    PostDetailPage,
     Categories,
     PostWidget,
     Author,
-    Comments,
-    CommentsForm,
 } from '@/components';
 import { getPostDetails, getPosts } from '@/services/blogAPI';
-import { AdjacentPosts } from '../../sections';
 import { PostDetailType, PostNode } from '@/services/types';
 
 interface PostDetailsProps {
     post: PostDetailType;
 }
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const data = await getPostDetails(params?.slug as string);
+
+    return {
+        props: {
+            post: data,
+        },
+    };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const posts = await getPosts();
+
+    return {
+        paths: posts.postsConnection.edges.map(({ node }: { node: PostNode }) => ({
+            params: { slug: node.slug }
+        })),
+        fallback: true,
+    };
+};
 
 const PostDetails: React.FC<PostDetailsProps> = ({ post }) => {
     const router = useRouter();
@@ -24,33 +42,14 @@ const PostDetails: React.FC<PostDetailsProps> = ({ post }) => {
         return <div>Loading ...</div>;
     }
 
-    export const getStaticProps: GetStaticProps = async ({ params }) => {
-        const data = await getPostDetails(params?.slug as string);
 
-        return {
-            props: {
-                post: data,
-            },
-        };
-    };
-
-    export const getStaticPaths: GetStaticPaths = async () => {
-        const posts = await getPosts();
-
-        return {
-            paths: posts.postsConnection.edges.map(({ node }: { node: PostNode }) => ({
-                params: { slug: node.slug }
-            })),
-            fallback: true,
-        };
-    };
 
     return (
         <>
             <div className="container mx-auto px-10 mb-8">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                     <div className="col-span-1 lg:col-span-8">
-                        <PostDetail post={post} />
+                        <PostDetailPage post={post} />
                         <Author author={post.author} />
                         <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
                         <CommentsForm slug={post.slug} />
